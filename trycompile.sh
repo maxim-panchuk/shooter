30753bd90de81c73203e14add78db98358dae863
maksimpancuk <maksim.panchuk29@gmail.com>
2022-09-18 22:15:15 +0300

    Удалены параметры подключения по ssh, добавлен sh скрпит

--- trycompile.sh
+++ trycompile.sh
@@ -1,5 +1,5 @@
 
-ARG="2""p"
-git log | grep commit | head -2 | awk '{print $2}' | sed -n $ARG
+#ARG="2""p"
+#git log | grep commit | head -2 | awk '{print $2}' | sed -n $ARG
 
-git log | grep commit
+git log | grep commit | awk '{print $2}'
