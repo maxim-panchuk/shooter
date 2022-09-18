5973b4d43914832b877697b9e82327564d329f2b
maksimpancuk <maksim.panchuk29@gmail.com>
2022-09-18 21:39:22 +0300

    Добавлен сборщик на Ant (compile, build, test)

---
+++ src/main/java/com/panchuk/shooter/business/test/Const.java
@@ -0,0 +1,8 @@
+package com.panchuk.shooter.business.test;
+
+public class Const {
+    public static double RAD_1 = 1.0d;
+    public static double RAD_2 = 2.0d;
+    public static double RAD_3 = 3.0d;
+
+}
