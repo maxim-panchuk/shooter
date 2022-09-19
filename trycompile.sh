d12992222bb2c581b066c5b9d3e9fc1727e180f8
maksimpancuk <maksim.panchuk29@gmail.com>
2022-09-19 14:45:51 +0300

    Добавлен target для build.xml "History"

--- trycompile.sh
+++ trycompile.sh
@@ -1,5 +1,25 @@
+#!/bin/bash
+ant clean
+N=$(git rev-list --count HEAD master )
+for (( counter = 1; counter <= $N; counter++ ));
+do
+  ARG="$counter""p"
+  COMMIT_HASH=$(git log | grep "commit " | head -$N | awk '{print $2}' | sed -n $ARG)
+  git checkout "$COMMIT_HASH"
+  git checkout master build.xml
+  git checkout master temps.properties
+  ant build
 
-#ARG="2""p"
-#git log | grep commit | head -2 | awk '{print $2}' | sed -n $ARG
-
-git log | grep commit | awk '{print $2}'
+  if  [ -d ./build/ ];
+  then
+    echo "BUILD SUCCESSFUL OF COMMIT: " "$COMMIT_HASH"
+    touch file
+    ((N = N + 1))
+    ((counter = counter + 1))
+    ARG1="$counter""p"
+    COMMIT_HASH_NEXT=$(git log | grep "commit " | head -$N | awk '{print $2}' | sed -n $ARG1)
+    echo "NEXT COMMIT HAS IS: " "$COMMIT_HASH_NEXT"
+    git diff "$COMMIT_HASH" "$COMMIT_HASH_NEXT" > file
+    break
+  fi
+done
